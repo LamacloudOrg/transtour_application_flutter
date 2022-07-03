fd94d0976f877803bc5fdac82e91aa9edde747bf
Pablo Ocanto Malianni <ARX0001429_prosegur>
2022-07-03 20:45:21 -0300

    agregado de service

---
+++ lib/src/service/http_service.dart
@@ -0,0 +1,42 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:dio/adapter.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter/widgets.dart';
+import 'package:transtour_application/src/model/authentication.dart';
+
+class HttpService extends ChangeNotifier {
+  final String BASE_URL = 'https://transtour.com.ar:8080/api';
+  bool isAuthenticated = false;
+  Map<String, String> headers = {};
+  late Dio dio;
+
+  HttpService() {
+    headers.putIfAbsent('Content-type', () => 'application/json');
+    dio = Dio();
+
+    (dio.httpClientAdapter as DefaultHttpClientAdapter).onHttpClientCreate =
+        (HttpClient client) {
+      client.badCertificateCallback =
+          (X509Certificate cert, String host, int port) => true;
+      return client;
+    };
+  }
+
+  Future<void> authenticate(Authentication authentication) async {
+    try {
+      var response = await dio.post(
+        BASE_URL + '/service-user/v1/user/oauth/token',
+        // headers: headers,
+        data: json.encode(authentication),
+      );
+
+      var statusCode = response.statusCode ?? 0;
+      // TODO  retorna ok o error.
+    } catch (e) {
+      print(e.toString());
+    } finally {
+      //client.close();
+    }
+  }
+}
